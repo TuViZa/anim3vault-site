ac17897ecd90b9a291167386e4a6b71d01e1a34e
TuViZa <tusharzaware234@gmail.com>
2025-06-28 21:47:58 +0530

    Update fetch-videos.js

--- api/fetch-videos.js
+++ api/fetch-videos.js
@@ -1,7 +1,9 @@
 const API_KEY = process.env.YT_API_KEY;
+
 const PLAYLISTS = {
   donghua: "PLbTlJpronU8_GMDGyQlawguePVZVNnxNO",
-  news: "PLbTlJpronU89p4fsTtmbW_bNgcJonX2ae"
+  news: "PLbTlJpronU89p4fsTtmbW_bNgcJonX2ae",
+  anim3: "PLbTlJpronU8_-c2M_G7P4VwtBJTn1S0wk" // ✅ Anime playlist added here
 };
 
 const DONGHUA_TITLES = [
